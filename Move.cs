d64559fc67817eea29fcef8ada63309893cfd1d7
KimSH <112936661+KSH1974@users.noreply.github.com>
2022-09-25 11:24:42 +0900

    이동하는 기능 구현 회피기능 설계 Navmesh agent 사용

---
+++ Move.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Move : MonoBehaviour
+{
+
+
+    public float nSpeed;      // 나중에 캐릭터 Status 구조체 만들어서 참조하도록 할 것 
+    public NavMeshAgent agent;
+    private Vector3 movePoint; // 이동 위치 저장
+    public Camera mainCamera; // 메인 카메라
+    public Ray ray;
+    void Start()
+    {
+        //기본설정 초기화
+        nSpeed = 4.0f;
+
+        mainCamera = Camera.main;
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.speed = nSpeed;
+        agent.angularSpeed = 760.0f;
+        agent.stoppingDistance = 0;
+        agent.autoBraking = false;
+    }
+
+    void Update()
+    {
+
+        mainCamera.transform.position = new Vector3(this.transform.position.x, 25, this.transform.position.z); //test용 카메라 위치 
+        ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButton(1))
+        {
+            Move_to(MovePointReturn(ray)); 
+        }
+
+    }
+
+
+
+
+    // 추후 아래 함수들 따로 묶어서 기능관리, 유지보수 용이 
+    void Move_to(Vector3 movePoint)
+    {
+        agent.isStopped = false; //test 명령어 Dodging func완성 이후 폐기 
+        agent.SetDestination(movePoint);
+        
+    }
+    Vector3 MovePointReturn(Ray ray)
+    {
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            movePoint = raycastHit.point;
+            Debug.Log("movePoint : " + movePoint.ToString());
+            Debug.Log("맞은 객체 : " + raycastHit.transform.name);
+
+        }
+        return movePoint;
+    }
+    public Ray getRay()
+    {
+        return ray;
+    }
+
+}
+
