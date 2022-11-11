75b67c97d76ba75ed071b013a4c95408f606f26f
HHojin <hogin0616@gmail.com>
2022-11-11 19:27:39 +0900

    Dodge anim update
    회피 애니메이션 오류 수정

--- OneManArmy/Assets/Scripts/Enemy/EnemyHit.cs
+++ OneManArmy/Assets/Scripts/Enemy/EnemyHit.cs
@@ -7,11 +7,11 @@ public class EnemyHit : MonoBehaviour
    public Transform Enemy;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "Player")
+        if (other.gameObject.CompareTag("Player"))
         {
             Debug.LogWarning("Enemy's Hit");//利 单固瘤 贸府
-            other.GetComponent<PlayerStat>().TakeDamage(Enemy.GetComponent<EnemyStat>().Attack_power.GetStat());
-            Debug.LogWarning(other.GetComponent<PlayerStat>().Current_HP);
+            other.transform.parent.GetComponent<PlayerStat>().TakeDamage(Enemy.GetComponent<EnemyStat>().Attack_power.GetStat());
+            Debug.LogWarning(other.transform.parent.GetComponent<PlayerStat>().Current_HP);
         }
     }
 }
--- OneManArmy/Assets/Scripts/Enemy/Enemy_Attack.cs
+++ OneManArmy/Assets/Scripts/Enemy/Enemy_Attack.cs
@@ -17,7 +17,7 @@ public class Enemy_Attack: MonoBehaviour
     private void OnTriggerEnter(Collider other) //이거 그냥 거리 따라서 공격모션 나오는거로 할까? 다음주 회의내용
     {
       
-        if (other.gameObject.name == "Main_player")
+        if (other.gameObject.CompareTag("Player"))
         {
             Debug.LogWarning("Play Attack motion & stand");// 이동 멈추고 공격모션 나와야함 
             //공격 애니메이션 재생
@@ -28,7 +28,7 @@ public class Enemy_Attack: MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.name == "Main_player")
+        if (other.gameObject.CompareTag("Player"))
         {
             parent.GetComponent<EnemyAnimationControl>().AttackAnim(false); // 공격 범위 벗어났을 때 공격 취소
         }
